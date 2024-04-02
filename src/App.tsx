fb9f7c43da24279f7961e726389e224819791d05
Marco Vidali <marcovidali1@skiff.com>
2024-04-02 14:22:24 +0200

    Handle errors

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Toaster } from "react-hot-toast";
 
 import Shorten from "./pages/Shorten";
 import AppLayout from "./ui/AppLayout";
@@ -9,16 +10,22 @@ const queryClient = new QueryClient();
 
 function App() {
     return (
-        <QueryClientProvider client={queryClient}>
-            <BrowserRouter>
-                <Routes>
-                    <Route element={<AppLayout />}>
-                        <Route path="/" element={<Shorten />} />
-                        <Route path="*" element={<Redirect />} />
-                    </Route>
-                </Routes>
-            </BrowserRouter>
-        </QueryClientProvider>
+        <>
+            <Toaster
+                toastOptions={{ className: "bg-base-100 text-base-content" }}
+            />
+
+            <QueryClientProvider client={queryClient}>
+                <BrowserRouter>
+                    <Routes>
+                        <Route element={<AppLayout />}>
+                            <Route path="/" element={<Shorten />} />
+                            <Route path="*" element={<Redirect />} />
+                        </Route>
+                    </Routes>
+                </BrowserRouter>
+            </QueryClientProvider>
+        </>
     );
 }
 
--- src/features/shorten/useCreateRedirect.ts
+++ src/features/shorten/useCreateRedirect.ts
@@ -1,5 +1,6 @@
 import { useMutation } from "@tanstack/react-query";
 import { createRedirect as apiCreateRedirect } from "../../services/apiRedirects";
+import toast from "react-hot-toast";
 
 interface ParameterTypes {
     path: string;
@@ -10,6 +11,8 @@ export function useCreateRedirect() {
     const { mutate: createRedirect, isPending } = useMutation({
         mutationFn: ({ path, redirect }: ParameterTypes) =>
             apiCreateRedirect(path, redirect),
+        onError: () =>
+            toast.error("An error occurred while creating your shortened link"),
     });
 
     return { createRedirect, isPending };
